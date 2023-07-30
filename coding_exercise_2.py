76a8dc027a7aeb877d5281782e6780df53eb0194
Aisosa Aimufua <aisosaj@gmail.com>
2023-07-30 20:04:18 +0000

    coding exercise 2

--- coding_exercise_2.py
+++ coding_exercise_2.py
@@ -1,3 +1,5 @@
-"""
-Your module description
-"""
+income = 250_000
+lowtaxland_rate = 0.5
+ripoffland_rate = 0.43
+
+print ("your income is", income, "and you woud pay", income + lowtaxland_rate, "income tax in lowtaxland or", income * ripoffland_rate, "income tax in ripeoffland, You would save", income * ripoffland_rate - income * lowtaxland_rate, "ny paying taxes in lowtaxland!")
