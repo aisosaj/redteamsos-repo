e385ebf2fd20677edafc4fadd407a930eacc4d35
Aisosa Aimufua <aisosaj@gmail.com>
2023-07-30 21:03:11 +0000

    coding exercise 3

---
+++ coding_exercise_3.py
@@ -0,0 +1,7 @@
+print ("what is your login?")
+login = input ()
+print ("Hello,", login)
+print ("what language do you speak?")
+language = input()
+print ("Your login is", login, "and you speak", language)
+
