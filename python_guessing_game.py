b5b3a2e8cdcad8353de60c8679ee79f7175da715
Aisosa Aimufua <aisosaj@gmail.com>
2023-08-03 00:07:38 +0000

    python_guessing_game v1

--- python_guessing_game.py
+++ python_guessing_game.py
@@ -1,3 +1,11 @@
-"""
-Your module description
-"""
+release_year = int(input("When was python 1.0 released?: "))
+
+if release_year > 1994:
+    print("It was earlier than that!")
+    int(input("When was python 1.0 released?: "))
+if release_year < 1994:
+    print("It was later than that!")
+    int(input("When was python 1.0 released?: "))
+if release_year == 1994:
+    print("Correct. That is the correct year")
+    
